82637da09083d8e33cd0d51439bd972254fb35bc
Didiroro <llsource12@naver.com>
2018-11-04 11:39:33 +0900

    React Components Task 3

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,16 +3,31 @@ import { Card, CardTitle, CardBody, CardImg, CardText } from 'reactstrap';
 
 class DishDetailComponent extends Component {
     render() {
+        const comments = this.props.selectedDish.comments.map((comment) => {
+            return (
+                <div className="row m-1" key={comment.id}>
+                    <div className="m-2">{comment.comment}</div>
+                    <div className="m-2">-- {comment.author}, {comment.date}</div>
+                </div>
+            );
+        })
+
         return (
-            <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg width="100%" src={this.props.selectedDish.image} alt={this.props.selectedDish.name}></CardImg>
-                    <CardBody>
-                        <CardTitle>{this.props.selectedDish.name}</CardTitle>
-                        <CardText style={{color: '#353535'}}>{this.props.selectedDish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
+            <React.Fragment>
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={this.props.selectedDish.image} alt={this.props.selectedDish.name}></CardImg>
+                        <CardBody>
+                            <CardTitle>{this.props.selectedDish.name}</CardTitle>
+                            <CardText style={{color: '#353535'}}>{this.props.selectedDish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <h2>Comments</h2>
+                    {comments}
+                </div>
+            </React.Fragment>
         );
     }
 }
