5f7ac2a8963f18faef717596698e733dbeed70ac
Didiroro <llsource12@naver.com>
2018-11-08 20:23:16 +0900

    Functional Components (again)

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -8,63 +8,51 @@ import {
 } from 'reactstrap';
 
 function RenderDish({ dish }) {
-    if (dish === undefined) {
-        return (
-            <div></div>
-        );
-    } else {
-        return (
-            <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        );
-    }
+    return (
+        <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
 
 function RenderComments({ comments }) {
-    if (comments === undefined) {
+    const comment = comments.comments.map((comment) => {
         return (
-            <div></div>
-        );
-    } else {
-        const comment = comments.comments.map((comment) => {
-            return (
-                <div className="row m-1" key={comment.id}>
-                    <div className="m-2">{comment.comment}</div>
-                    <div className="m-2">
-                        -- {comment.author},
-                        {
-                            new Intl.DateTimeFormat(
-                                'en-US',
-                                { year: 'numeric', month: 'short', day: '2-digit' }
-                            ).format(new Date(Date.parse(comment.date)))
-                        }
-                    </div>
+            <div className="row m-1" key={comment.id}>
+                <div className="m-2">{comment.comment}</div>
+                <div className="m-2">
+                    -- {comment.author},
+                    {
+                        new Intl.DateTimeFormat(
+                            'en-US',
+                            { year: 'numeric', month: 'short', day: '2-digit' }
+                        ).format(new Date(Date.parse(comment.date)))
+                    }
                 </div>
-            );
-        });
-
-        return (
-            <div className="col-12 col-md-5 m-1">
-                <h2>Comments</h2>
-                {comment}
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="col-12 col-md-5 m-1">
+            <h2>Comments</h2>
+            {comment}
+        </div>
+    );
 }
 
 const DishDetail = (props) => {
     return (
         <div className="container" >
             <div className="row">
-                <RenderDish dish={props.dish} />
-                <RenderComments comments={props.dish} />
+                { props.dish && <RenderDish dish={props.dish} /> }
+                { props.dish && <RenderComments comments={props.dish} /> }
             </div>
         </div>
     );
