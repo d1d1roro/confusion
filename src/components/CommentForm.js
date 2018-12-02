65dc708b8e66131d9f0316349403a0caf6ad0bb4
Didiroro <llsource12@naver.com>
2018-12-02 17:11:59 +0900

    React Redux Task2

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, Button, Row, Label } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 class CommentForm extends Component {
     constructor(props) {
@@ -30,8 +31,37 @@ class CommentForm extends Component {
                 
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
-                    <ModalBody>
-
+                    <ModalBody className="ml-3 mr-3">
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="Rating">Rating</Label>
+                                <Control.select model=".Rating" name="Rating" id="Rating"
+                                    className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text model=".author" id="author" name="author"
+                                    placeholder="Your Name"
+                                    className="form-control"
+                                />
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="comment">Comment</Label>
+                                <Control.textarea model=".comment" id="comment" name="comment"
+                                    rows="6"
+                                    className="form-control" 
+                                />
+                            </Row>
+                            <Row className="form-group">
+                                <Button type="submit" color="primary">Submit</Button>
+                            </Row>
+                        </LocalForm>
                     </ModalBody>
                 </Modal>
             </>
